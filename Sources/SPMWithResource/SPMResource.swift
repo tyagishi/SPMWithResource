ba30fbaedd880cac834a51a1eb894ccc708c1809
Tomoaki Yagishita <tyagishi@gmail.com>
2020-07-29 10:17:00 +0900

    add SPM

---
+++ Sources/SPMWithResource/SPMResource.swift
@@ -0,0 +1,13 @@
+//
+//  SPMResource.swift
+//  ResourceSPM
+//
+//  Created by Tomoaki Yagishita on 2020/07/29.
+//
+
+import Foundation
+import SwiftUI
+
+func SPMResource(name: String) -> Image {
+    return Image(name)
+}
